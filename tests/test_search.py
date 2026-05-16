950800e8116f83b43836d68b77997c05b5ded782
Anna <148694721+maksimovann@users.noreply.github.com>
2026-05-16 16:12:44 +0300

    Create test_search.py

---
+++ tests/test_search.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import Page, expect
+
+
+def test_products_page_open(page: Page):
+    page.goto("https://www.saucedemo.com/")
+
+    page.fill("#user-name", "standard_user")
+    page.fill("#password", "secret_sauce")
+
+    page.click("#login-button")
+
+    expect(
+        page.locator(".inventory_list")
+    ).to_be_visible()
